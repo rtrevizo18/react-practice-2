8c1926622fe188cbe2734728134c7bdf46e807ae
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-16 13:16:19 -0500

    feat: Add basic routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
 
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
