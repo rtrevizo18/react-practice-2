30ea71773ed44fe9cd02c9916a61d7250a4d7972
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-16 19:05:10 -0500

    feat: Add basic user styling and structure components

--- src/users/pages/Users.jsx
+++ src/users/pages/Users.jsx
@@ -1,5 +1,16 @@
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image:
+        "https://as1.ftcdn.net/v2/jpg/02/43/12/34/1000_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
