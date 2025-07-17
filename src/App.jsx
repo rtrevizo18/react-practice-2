cf0a41cc2261b9a0c7817ab8b12c7313fcc40731
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-16 23:29:28 -0500

    feat: add basic header + styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,23 @@ import "./App.css";
 
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
