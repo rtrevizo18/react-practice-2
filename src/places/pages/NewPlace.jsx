c6e70faab7048161d8f62b58c65c27ac5eec5a71
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-18 13:48:50 -0500

    feat: Add input styling + form data handling

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -4,7 +4,13 @@ import "./NewPlace.css";
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter valid title."
+      />
     </form>
   );
 };
