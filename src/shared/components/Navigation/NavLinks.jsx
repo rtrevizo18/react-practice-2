d89969e2a8c4a6461f82efbd4263bda8b4bbacbb
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-19 15:43:30 -0500

    feat: Add logout feature when user is authenticated

--- src/shared/components/Navigation/NavLinks.jsx
+++ src/shared/components/Navigation/NavLinks.jsx
@@ -31,6 +31,11 @@ const NavLinks = (props) => {
           <NavLink to="/auth">AUTHENTICATE</NavLink>
         </li>
       )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
     </ul>
   );
 };
