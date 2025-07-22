917fb9872fa601227be0d1a2e56131f08c96d2eb
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-21 21:31:00 -0500

    feat: Configure login to API POST request

--- src/users/pages/Auth.jsx
+++ src/users/pages/Auth.jsx
@@ -65,10 +65,33 @@ const Auth = () => {
   const placeSubmitHandler = async (event) => {
     event.preventDefault();
 
+    setIsLoading(true);
+
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5001/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5001/api/users/signup", {
           method: "POST",
           headers: {
@@ -85,11 +108,9 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (err) {
-        console.log(err);
         setIsLoading(false);
         setError(err.message || "Something went wrong, please try again.");
       }
