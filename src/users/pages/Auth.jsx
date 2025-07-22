3779bfccef1f0d98d77adc0f3122e49f69a9afaf
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-21 21:26:16 -0500

    feat: Add error + loading handling for authentication

--- src/users/pages/Auth.jsx
+++ src/users/pages/Auth.jsx
@@ -2,6 +2,8 @@ import { useState, useContext } from "react";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 // Logic Helper functions
 import {
@@ -19,6 +21,8 @@ const Auth = () => {
   const auth = useContext(AuthContext);
 
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -64,6 +68,7 @@ const Auth = () => {
     if (isLoginMode) {
     } else {
       try {
+        setIsLoading(true);
         const response = await fetch("http://localhost:5001/api/users/signup", {
           method: "POST",
           headers: {
@@ -77,58 +82,71 @@ const Auth = () => {
         });
 
         const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
         console.log(responseData);
+        setIsLoading(false);
+        auth.login();
       } catch (err) {
         console.log(err);
-        return;
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
       }
     }
-    auth.login();
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <Card className="place-form">
-      <form onSubmit={placeSubmitHandler}>
-        <h2 style={{ textAlign: "center" }}>
-          {isLoginMode ? "Log in" : "Sign up"}
-        </h2>
-        {!isLoginMode && (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className="place-form">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <form onSubmit={placeSubmitHandler}>
+          <h2 style={{ textAlign: "center" }}>
+            {isLoginMode ? "Log in" : "Sign up"}
+          </h2>
+          {!isLoginMode && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name."
+              onInput={inputHandler}
+            />
+          )}
           <Input
+            id="email"
             element="input"
-            id="name"
-            type="text"
-            label="Your Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a name."
+            type="email"
+            label="Email Address"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email (Ex. example@org.com"
             onInput={inputHandler}
           />
-        )}
-        <Input
-          id="email"
-          element="input"
-          type="email"
-          label="Email Address"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email (Ex. example@org.com"
-          onInput={inputHandler}
-        />
-        <Input
-          id="password"
-          element="input"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid password"
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? "LOG IN" : "SIGN UP"}
+          <Input
+            id="password"
+            element="input"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password"
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? "LOG IN" : "SIGN UP"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? "SIGN UP" : "LOG IN"}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        SWITCH TO {isLoginMode ? "SIGN UP" : "LOG IN"}
-      </Button>
-    </Card>
+      </Card>
+    </>
   );
 };
 
