d87e9fac4c598e7a90055c88c6e24c8bec89a71c
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-28 10:50:43 -0500

    feat: Add new logo to favicon and navi bar

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -7,6 +7,28 @@ import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 
+const LogoSVG = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      fill="white"
+      class="bi bi-pin-map"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z"
+      />
+      <path
+        fill-rule="evenodd"
+        d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"
+      />
+    </svg>
+  );
+};
+
 const MainNavigation = () => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
   const openDrawerHandler = () => {
@@ -34,9 +56,12 @@ const MainNavigation = () => {
           <span />
           <span />
         </button>
-        <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
-        </h1>
+        <div className="main-navigation__left">
+          <LogoSVG />
+          <h1 className="main-navigation__title">
+            <Link to="/">YourPlaces</Link>
+          </h1>
+        </div>
         <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
